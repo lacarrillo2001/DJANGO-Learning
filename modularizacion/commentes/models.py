3cf442a78fcee29a38a6efd6012788b01d48901e
lacarrillo2001 <lacarrillo6@espe.edu.ec>
2025-12-03 13:27:21 -0500

    firs model 'comments' and migration file

--- modularizacion/commentes/models.py
+++ modularizacion/commentes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+     name = models.CharField(max_length=255,null=False)
+     score = models.IntegerField(default=3)
+     Comment_text = models.TextField(max_length=10000,null=True)
+     date_created = models.DateTimeField(null=True)
+     signature = models.CharField(max_length=255,default='Anonimo')
+
+     def __str__(self):
+          return self.name
